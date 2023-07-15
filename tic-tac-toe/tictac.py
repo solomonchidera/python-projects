c3a686d9ba9f426e91faf8ff650170bdcb849e8a
SolomonChidera <solochidera10@gmail.com>
2023-07-15 15:29:31 +0100

    starting the game

---
+++ tic-tac-toe/tictac.py
@@ -0,0 +1,33 @@
+#  CREATING OUR BOARD
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-",]
+
+first = "X"
+winner = None
+gameRunning = True
+
+# PRINTING THE BOARD
+def printBoard(board):
+        print(board[0] + " | " + board[1] + " | " + board[2])
+        print("----------")
+        print(board[3] + " | " + board[4] + " | " + board[5])
+        print("----------")
+        print(board[6] + " | " + board[7] + " | " + board[8])
+printBoard(board)
+
+
+# TAKE THE PLAYER INPUT 
+
+
+
+
+# CHECK FOR WIN OR TIE
+
+
+
+# SWITCH THE PLAYER
+
+
+
+# CHECK FOR WIN OR TIE AGAIN
